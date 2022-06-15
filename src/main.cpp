e600b018301cbc54b136b14e645e56765da7f201
Zacaruca <zacaruca@gmail.com>
2022-06-15 20:50:58 +0200

    inicio

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+
+// Declaracion de variables
+
+// Semaforo 1
+const byte S1_Coches_Rojo = 1;    // pin para el semaforo 1 Rojo
+const byte S1_Coches_Ambar = 1;   // pin para el semaforo 1 Ambar
+const byte S1_Coches_Verde = 1;   // pin para el semaforo 1 Verde
+const byte S1_Peatones_Rojo = 1;  // pin para el semaforo 1 Rojo
+const byte S1_Peatones_Verde = 1; // pin para el semaforo 1 Verde
+
+// Semaforo 2
+const byte S2_Coches_Rojo = 1;    // pin para el semaforo 2 Rojo
+const byte S2_Coches_Ambar = 1;   // pin para el semaforo 2 Ambar
+const byte S2_Coches_Verde = 1;   // pin para el semaforo 2 Verde
+const byte S2_Peatones_Rojo = 1;  // pin para el semaforo 2 Rojo
+const byte S2_Peatones_Verde = 1; // pin para el semaforo 2 Verde
+
+// Semaforo 3
+const byte S3_Coches_Rojo = 1;    // pin para el semaforo 3 Rojo
+const byte S3_Coches_Ambar = 1;   // pin para el semaforo 3 Ambar
+const byte S3_Coches_Verde = 1;   // pin para el semaforo 3 Verde
+const byte S3_Peatones_Rojo = 1;  // pin para el semaforo 3 Rojo
+const byte S3_Peatones_Verde = 1; // pin para el semaforo 3 Verde
+
+// Semaforo 4
+const byte S4_Coches_Rojo = 1;    // pin para el semaforo 4 Rojo
+const byte S4_Coches_Ambar = 1;   // pin para el semaforo 4 Ambar
+const byte S4_Coches_Verde = 1;   // pin para el semaforo 4 Verde
+const byte S4_Peatones_Rojo = 1;  // pin para el semaforo 4 Rojo
+const byte S4_Peatones_Verde = 1; // pin para el semaforo 4 Verde
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
